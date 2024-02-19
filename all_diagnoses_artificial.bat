35fdf1a216cf0b9717dea06e457ef2c5d99ef52f
bhoferTU <bhofer@ist.tugraz.at>
2024-02-19 10:39:28 +0100

    update according to reviewer comments

---
+++ all_diagnoses_artificial.bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal
+
+REM Set the path to your Java executable
+set JAVA_PATH="C:\Program Files (x86)\Common Files\Oracle\Java\javapath\java.exe"
+
+REM Set the path to your JAR file
+set JAR_PATH="C:\Users\inica\Documents\GitHub\SFLspreadsheets\MBDebugger.jar"
+
+REM Set the parameter you want to pass to the JAR file
+set CONFIG_FILE=all_diagnoses_artificial.config
+
+REM Run the JAR file with the specified configuration file
+%JAVA_PATH% -jar %JAR_PATH% %CONFIG_FILE%
+
+endlocal
+pause
---
+++ min_diagnoses_artificial_10RUNS.bat
@@ -0,0 +1,35 @@
+@echo off
+setlocal
+
+REM Set the path to your Java executable
+set JAVA_PATH="C:\Program Files (x86)\Common Files\Oracle\Java\javapath\java.exe"
+
+REM Set the path to your JAR file
+set JAR_PATH="C:\Users\inica\Documents\GitHub\SFLspreadsheets\MBDebugger.jar"
+
+REM Set the parameter you want to pass to the JAR file
+set CONFIG_FILE1=min_diagnoses_artificial_RUN1.config
+set CONFIG_FILE2=min_diagnoses_artificial_RUN2.config
+set CONFIG_FILE3=min_diagnoses_artificial_RUN3.config
+set CONFIG_FILE4=min_diagnoses_artificial_RUN4.config
+set CONFIG_FILE5=min_diagnoses_artificial_RUN5.config
+set CONFIG_FILE6=min_diagnoses_artificial_RUN6.config
+set CONFIG_FILE7=min_diagnoses_artificial_RUN7.config
+set CONFIG_FILE8=min_diagnoses_artificial_RUN8.config
+set CONFIG_FILE9=min_diagnoses_artificial_RUN9.config
+set CONFIG_FILE10=min_diagnoses_artificial_RUN10.config
+
+REM Run the JAR files with the specified configuration file
+%JAVA_PATH% -jar %JAR_PATH% %CONFIG_FILE1%
+%JAVA_PATH% -jar %JAR_PATH% %CONFIG_FILE2%
+%JAVA_PATH% -jar %JAR_PATH% %CONFIG_FILE3%
+%JAVA_PATH% -jar %JAR_PATH% %CONFIG_FILE4%
+%JAVA_PATH% -jar %JAR_PATH% %CONFIG_FILE5%
+%JAVA_PATH% -jar %JAR_PATH% %CONFIG_FILE6%
+%JAVA_PATH% -jar %JAR_PATH% %CONFIG_FILE7%
+%JAVA_PATH% -jar %JAR_PATH% %CONFIG_FILE8%
+%JAVA_PATH% -jar %JAR_PATH% %CONFIG_FILE9%
+%JAVA_PATH% -jar %JAR_PATH% %CONFIG_FILE10%
+
+endlocal
+pause
